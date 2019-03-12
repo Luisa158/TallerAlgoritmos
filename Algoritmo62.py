fdbb9374b260b3b3f07e196c0c3086adcd114c75
Luisa158 <luisa.monsalve.2019upb.edu.co>
2019-03-12 15:17:31 -0500

    Corrección orden

--- Algoritmo62.py
+++ Algoritmo62.py
@@ -1,9 +1,10 @@
 
 suma = 0
 
-for i in range (0,10):
-    x=int(input("Por favor, ingrese un número: "))
-    y=int(input("Por favor, ingrese otro número: "))
+x = int(input("Por favor, ingrese un número: "))
+y = int(input("Por favor, ingrese un número menor al primero: "))
+for i in range (y,x):
+
     suma = suma+x
 print("La suma es: ",suma)
 
