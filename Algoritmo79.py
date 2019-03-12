96603f69a161f76c6b068475f22d3041f4fa3554
Luisa158 <luisa.monsalve.2019upb.edu.co>
2019-03-12 12:10:21 -0500

    Piramide números

--- Algoritmo79.py
+++ Algoritmo79.py
@@ -1,14 +1,11 @@
-
-n=int(input("Por favor, Ingrese el número 1: "))
-for i in range (n+1):
-    for l in range (0,i):
-        print(1)
-        print(1,2)
-        print(1,2,3)
-        print(1,2,3,4)
-        print(1,2,3,4,5)
-        print(1, 2, 3, 4, 5,6)
-        print(1, 2, 3, 4, 5,6,7)
-        print(1, 2, 3, 4, 5,6,7,8)
-        print(1, 2, 3, 4, 5, 6, 7, 8,9)
-        print(1, 2, 3, 4, 5, 6, 7, 8,9,10)
+print(1)
+print(1,2)
+print(1,2,3)
+print(1,2,3,4)
+print(1,2,3,4,5)
+print(1, 2, 3, 4, 5,6)
+print(1, 2, 3, 4, 5,6,7)
+print(1, 2, 3, 4, 5,6,7,8)
+print(1, 2, 3, 4, 5, 6, 7, 8,9)
+print(1, 2, 3, 4, 5, 6, 7, 8,9,10)
+        
